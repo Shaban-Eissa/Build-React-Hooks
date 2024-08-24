6e25f26eeb425439977b4a73bf88d75dbd282acc
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-24 09:10:07 +0300

    ✨ New Feature: Update useState hook to trigger re-render on state change

--- src/hooks/useState/index.ts
+++ src/hooks/useState/index.ts
@@ -1,5 +1,11 @@
+import { render } from "../../main";
+
 export function useState<State>(initialState: State) {
   let state = initialState;
-  const setState = (newState: State) => (state = newState);
+  const setState = (newState: State) => {
+    state = newState;
+
+    render();
+  };
   return [state, setState] as const;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+const rootEl = document.createElement("div");
+document.body.append(rootEl);
+const appRoot = createRoot(rootEl);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export function render() {
+  appRoot.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+render();
